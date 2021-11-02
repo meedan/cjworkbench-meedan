bc6a5306a5df40acd49d2d50fbb15a2656bcbe11
Martin Peck <martin@meedan.com>
2021-11-02 11:28:45 -0700

    Update storage setup to support configurable region and account.

--- cluster-setup/aws/01-storage.sh
+++ cluster-setup/aws/01-storage.sh
@@ -1,54 +1,54 @@
-aws s3 mb s3://user-files.$DOMAIN_NAME
-aws s3 mb s3://static.$DOMAIN_NAME
-aws s3 mb s3://stored-objects.$DOMAIN_NAME
-aws s3 mb s3://external-modules.$DOMAIN_NAME
-aws s3 mb s3://cached-render-results.$DOMAIN_NAME
-aws s3 mb s3://upload.$DOMAIN_NAME
+aws s3 mb --region $AWS_REGION s3://cjworkbench-user-files.$DOMAIN_NAME
+aws s3 mb --region $AWS_REGION s3://cjworkbench-static.$DOMAIN_NAME
+aws s3 mb --region $AWS_REGION s3://cjworkbench-stored-objects.$DOMAIN_NAME
+aws s3 mb --region $AWS_REGION s3://cjworkbench-external-modules.$DOMAIN_NAME
+aws s3 mb --region $AWS_REGION s3://cjworkbench-cached-render-results.$DOMAIN_NAME
+aws s3 mb --region $AWS_REGION s3://cjworkbench-upload.$DOMAIN_NAME
 
 # Uploads expire after 1d
 echo '{"Rules":[{"Expiration":{"Days":1},"Prefix":"","Status":"Enabled","AbortIncompleteMultipartUpload":{"DaysAfterInitiation":1}}]}' \
   > 1d-lifecycle.json
-aws s3api put-bucket-lifecycle-configuration \
-  --bucket upload.$DOMAIN_NAME \
-  --lifecycle-configuration file://$DIR/1d-lifecycle.json
+aws s3api put-bucket-lifecycle-configuration  --region $AWS_REGION \
+  --bucket cjworkbench-upload.$DOMAIN_NAME \
+  --lifecycle-configuration file:./1d-lifecycle.json
 rm -f 1d-lifecycle.json
 
 # Static-files server
 # Public-access...
-aws s3api put-bucket-acl \
-  --bucket static.$DOMAIN_NAME \
+aws s3api put-bucket-acl --region $AWS_REGION \
+  --bucket cjworkbench-static.$DOMAIN_NAME \
   --acl public-read
 # ... CORS...
-aws s3api put-bucket-policy \
-  --bucket static.$DOMAIN_NAME \
-  --policy file://<(echo '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":"*","Action":"s3:GetObject","Resource":"arn:aws:s3:::static.'$DOMAIN_NAME'/*"}]}')
-aws s3api put-bucket-cors \
-  --bucket static.$DOMAIN_NAME \
+aws s3api put-bucket-policy --region $AWS_REGION \
+  --bucket cjworkbench-static.$DOMAIN_NAME \
+  --policy file://<(echo '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":"*","Action":"s3:GetObject","Resource":"arn:aws:s3:::cjworkbench-static.'$DOMAIN_NAME'/*"}]}')
+aws s3api put-bucket-cors --region $AWS_REGION \
+  --bucket cjworkbench-static.$DOMAIN_NAME \
   --cors-configuration '{"CORSRules":[{"AllowedOrigins":["*"],"AllowedMethods":["GET","HEAD"]}]}'
 # ... SSL...
-aws acm request-certificate \
-  --domain-name static.$DOMAIN_NAME \
+aws acm request-certificate --region $AWS_REGION \
+  --domain-name cjworkbench-static.$DOMAIN_NAME \
   --validation-method DNS
-static_cert_arn="$(aws acm list-certificates | jq -r ".CertificateSummaryList[] | select(.DomainName == \"static.$DOMAIN_NAME\") | .CertificateArn")"
-echo "Go to https://console.aws.amazon.com/acm/home to generate the Route 53 record for '$static_cert_arn' ('static.$DOMAIN_NAME') ... and then wait up to 30min...."
-aws acm wait certificate-validated \
+static_cert_arn="$(aws acm list-certificates --region $AWS_REGION | jq -r ".CertificateSummaryList[] | select(.DomainName == \"cjworkbench-static.$DOMAIN_NAME\") | .CertificateArn")"
+echo "Go to https://console.aws.amazon.com/acm/home to generate the Route 53 record for '$static_cert_arn' ('cjworkbench-static.$DOMAIN_NAME') ... and then wait up to 30min...."
+aws acm wait certificate-validated --region $AWS_REGION \
   --certificate-arn "$static_cert_arn"
 # ... CDN...
 read -r -d '' cloudfront_distribution_config <<EOT
   {
-    "Comment": "static.$DOMAIN_NAME",
+    "Comment": "cjworkbench-static.$DOMAIN_NAME",
     "Enabled": true,
-    "CallerReference": "static.$DOMAIN_NAME",
+    "CallerReference": "cjworkbench-static.$DOMAIN_NAME",
     "Aliases": {
       "Quantity": 1,
-      "Items": ["static.$DOMAIN_NAME"]
+      "Items": ["cjworkbench-static.$DOMAIN_NAME"]
     },
     "Origins": {
       "Quantity": 1,
       "Items": [
         {
-          "Id": "static.$DOMAIN_NAME.s3.amazonaws.com",
-          "DomainName": "static.$DOMAIN_NAME.s3.amazonaws.com",
+          "Id": "cjworkbench-static.$DOMAIN_NAME.s3.amazonaws.com",
+          "DomainName": "cjworkbench-static.$DOMAIN_NAME.s3.amazonaws.com",
           "OriginPath": "",
           "S3OriginConfig": { "OriginAccessIdentity": "" }
         }
@@ -59,7 +59,12 @@ read -r -d '' cloudfront_distribution_config <<EOT
       "SSLSupportMethod": "sni-only"
     },
     "DefaultCacheBehavior": {
-      "TargetOriginId": "static.$DOMAIN_NAME.s3.amazonaws.com",
+      "TargetOriginId": "cjworkbench-static.$DOMAIN_NAME.s3.amazonaws.com",
+      "TrustedSigners": {
+        "Enabled": true,
+        "Quantity": 1,
+        "Items": ["$AWS_ACCOUNT_ID"]
+      },
       "ForwardedValues": {
         "QueryString": false,
         "Cookies": { "Forward": "none" },
@@ -73,6 +78,6 @@ read -r -d '' cloudfront_distribution_config <<EOT
     }
   }
 EOT
-aws cloudfront create-distribution \
+aws cloudfront create-distribution --region $AWS_REGION \
   --distribution-config "$cloudfront_distribution_config" \
   | jq  # avoid AWS's default pager
